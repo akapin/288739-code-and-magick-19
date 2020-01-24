7cbc88317a4cbddc1866c0ca85461e341f3f7358
Alex Kapin <alexander.kapin@outlook.com>
2020-01-24 10:47:42 +0300

    stats.js refactor 2

--- js/stat.js
+++ js/stat.js
@@ -5,8 +5,6 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
-var CLOUD_SHADOW_X = CLOUD_X + CLOUD_GAP;
-var CLOUD_SHADOW_Y = CLOUD_Y + CLOUD_GAP;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var MAX_BAR_HEIGHT = 150;
@@ -15,14 +13,15 @@ var HISTOGRAM_TOP_MARGIN = 80;
 var TIME_TEXT_MARGIN = 20;
 var NAME_TEXT_MARGIN = 10;
 var INTRO_TEXT_X_MARGIN = 20;
-var INTRO_TEXT_X = CLOUD_X + INTRO_TEXT_X_MARGIN;
 var CONGRATULATIONS_TEXT_Y_MARGIN = 30;
 var RESULTS_TEXT_Y_MARGIN = 50;
 var BLACK_COLOR = '#000';
 var WHITE_COLOR = '#fff';
 var RED_COLOR = 'rgba(255, 0, 0, 1)';
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-var TEXT_FONT = '16px PT Mono';
+var BLUE_COLOR_HUE_CODE = 240;
+var COLOR_LIGHTNESS_PERCENTAGE = 50;
+var FONT_PROPERTY = '16px PT Mono';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -30,11 +29,12 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var renderIntroText = function (ctx) {
+  var introTextX = CLOUD_X + INTRO_TEXT_X_MARGIN;
   ctx.fillStyle = BLACK_COLOR;
-  ctx.font = TEXT_FONT;
+  ctx.font = FONT_PROPERTY;
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', INTRO_TEXT_X, CONGRATULATIONS_TEXT_Y_MARGIN);
-  ctx.fillText('Список результатов:', INTRO_TEXT_X, RESULTS_TEXT_Y_MARGIN);
+  ctx.fillText('Ура вы победили!', introTextX, CONGRATULATIONS_TEXT_Y_MARGIN);
+  ctx.fillText('Список результатов:', introTextX, RESULTS_TEXT_Y_MARGIN);
 };
 
 var renderBar = function (ctx, x, y, width, height, color) {
@@ -42,35 +42,39 @@ var renderBar = function (ctx, x, y, width, height, color) {
   ctx.fillRect(x, y, width, height);
 };
 
+var getRandomTintOfColor = function (hue, lightness) {
+  return 'hsl(' + hue + ', ' + Math.floor(Math.random() * 101) + '%, ' + lightness + '%)';
+};
+
 var renderHistogram = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    var BAR_X = CLOUD_X + HISTOGRAM_LEFT_MARGIN + (BAR_WIDTH + BAR_GAP) * i;
-    var BAR_HEIGHT = (MAX_BAR_HEIGHT * times[i]) / (maxTime === 0 ? 1 : maxTime);
-    var BAR_Y = CLOUD_Y + HISTOGRAM_TOP_MARGIN + MAX_BAR_HEIGHT - BAR_HEIGHT;
-    var NAME_TEXT_Y = BAR_Y + BAR_HEIGHT + NAME_TEXT_MARGIN;
-    var TIME_TEXT_Y = BAR_Y - TIME_TEXT_MARGIN;
-    var BAR_COLOR = BLACK_COLOR;
-    var TEXT_COLOR = BLACK_COLOR;
+    var barX = CLOUD_X + HISTOGRAM_LEFT_MARGIN + (BAR_WIDTH + BAR_GAP) * i;
+    var barHeight = (MAX_BAR_HEIGHT * times[i]) / (maxTime === 0 ? 1 : maxTime);
+    var barY = CLOUD_Y + HISTOGRAM_TOP_MARGIN + MAX_BAR_HEIGHT - barHeight;
+    var nameTextY = barY + barHeight + NAME_TEXT_MARGIN;
+    var timeTextY = barY - TIME_TEXT_MARGIN;
+    var barColor = BLACK_COLOR;
+    var textColor = BLACK_COLOR;
 
-    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillStyle = textColor;
 
-    ctx.fillText(Math.round(times[i]), BAR_X, TIME_TEXT_Y);
-    ctx.fillText(names[i], BAR_X, NAME_TEXT_Y);
+    ctx.fillText(Math.round(times[i]), barX, timeTextY);
+    ctx.fillText(names[i], barX, nameTextY);
 
     if (names[i] === 'Вы') {
-      BAR_COLOR = RED_COLOR;
+      barColor = RED_COLOR;
     } else {
-      BAR_COLOR = 'hsl(240,' + Math.floor(Math.random() * 101) + '%, 50%)';
+      barColor = getRandomTintOfColor(BLUE_COLOR_HUE_CODE, COLOR_LIGHTNESS_PERCENTAGE);
     }
 
-    renderBar(ctx, BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT, BAR_COLOR);
+    renderBar(ctx, barX, barY, BAR_WIDTH, barHeight, barColor);
   }
 };
 
 var getMaxElement = function (arr) {
-  if (!arr || !arr.length) {
+  if (!arr && !arr.length) {
     return null;
   }
 
@@ -86,7 +90,9 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, CLOUD_SHADOW_COLOR);
+  var cloudShadowX = CLOUD_X + CLOUD_GAP;
+  var cloudShadowY = CLOUD_Y + CLOUD_GAP;
+  renderCloud(ctx, cloudShadowX, cloudShadowY, CLOUD_SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, WHITE_COLOR);
   renderIntroText(ctx);
   renderHistogram(ctx, names, times);
